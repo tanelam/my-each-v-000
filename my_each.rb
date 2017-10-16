5e500124a84bdf7598840624022937e3eebed3b3
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-16 16:53:59 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  if block_given?
+    x = 0
+    while x < array.length
+      yield(array[x])
+      x += 1
+    end
+    array
+end
+end
+
+my_each([]) do |y|
+  print y
 end
